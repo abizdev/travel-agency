c6be650dd07497d8689d25040cbd8630b46163de
Abdulaziz <abizlancer@gmail.com>
2023-11-09 14:38:45 +0500

    feat: category component base

---
+++ src/components/category/category.component.jsx
@@ -0,0 +1,14 @@
+import './category.style.scss'
+
+const Category = () => {
+  return (
+    <section className='category'>
+      <div className='category__content container'>
+        <h4 className='sub--title'></h4>
+        <h2 className='title'></h2>
+      </div>
+    </section>
+  )
+}
+
+export default Category
--- src/components/partners/partners.component.jsx
+++ src/components/partners/partners.component.jsx
@@ -3,11 +3,11 @@ import './partners.styles.scss'
 const Partners = () => {
   return (
     <section className="partners">
-      <img src="/public/images/partners/partner1.png" alt="partner" />
-      <img src="/public/images/partners/partner2.png" alt="partner" />
-      <img src="/public/images/partners/partner3.png" alt="partner" />
-      <img src="/public/images/partners/partner4.png" alt="partner" />
-      <img src="/public/images/partners/partner5.png" alt="partner" />
+      <img src="/images/partners/partner1.png" alt="partner" />
+      <img src="/images/partners/partner2.png" alt="partner" />
+      <img src="/images/partners/partner3.png" alt="partner" />
+      <img src="/images/partners/partner4.png" alt="partner" />
+      <img src="/images/partners/partner5.png" alt="partner" />
     </section>
   )
 }
