e1e141c539fe07440b8582a98659fb4b725b62cc
Abdulaziz <abizlancer@gmail.com>
2023-11-10 11:57:25 +0500

    feat: specials section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/header/header.component"
 import Partners from "./components/partners/partners.component"
 import Category from "./components/category/category.component"
 import Banner from "./components/banner/banner.component"
+import Specials from "./components/specials/specials.component"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <Partners />
       <Category />
+      <Specials />
       <Banner />
     </>
   )
---
+++ src/components/specials/specials.component.jsx
@@ -0,0 +1,17 @@
+import './specials.styles.scss'
+
+const Specials = () => {
+  return (
+    <section className="specials container">
+      <img src="/public/images/special.png" alt="special" />
+      <div className="specials__info">
+        <h4 className="sub--title">Honeymoon Specials</h4>
+        <h2 className="title">Our Romantic Tropical Destinations</h2>
+        <p>Et labore harum non nobis ipsum eum molestias mollitia et corporis praesentium a laudantium internos. Non quis eius quo eligendi corrupti et fugiat nulla qui soluta recusandae in maxime quasi aut ducimus illum aut optio quibusdam!</p>
+        <a href="#!" className="link-btn">Contacts</a>
+      </div>
+    </section>
+  )
+}
+
+export default Specials
