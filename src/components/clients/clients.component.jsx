9daf9b96fd4f91ff3572415a749658933afdc9aa
Abdulaziz <abizlancer@gmail.com>
2023-11-11 21:08:57 +0500

    feat: clients section slider and response

--- src/components/clients/clients.component.jsx
+++ src/components/clients/clients.component.jsx
@@ -16,31 +16,39 @@ const Clients = () => {
       <div className="clients__content">
         <h4 className="sub--title">Promotion</h4>
         <h2 className="title">See What Our Clients Say About Us</h2>
-        <Swiper
-          spaceBetween={30}
-          slidesPerView={1}
-          cssMode={true}
-          navigation={true}
-          className="clients--slides"
-          pagination={{ clickable: true }}
-          modules={[Navigation, Pagination]}
-        >
-          <SwiperSlide className="clients--slide">
-            <img src="/public/images/client.png" alt="client" />
-            <p>Vel officiis dolor ea illo aut eligendi ullam non laudantium magnam et recusandae molestiae sit iure unde aut voluptate quaerat. Id sunt provident quo possimus impedit vel doloremque obcaecati qui ullam consectetur et ipsum omnis.</p>
-            <span>Christine Beckam - Designer</span>
-          </SwiperSlide>
-          <SwiperSlide className="clients--slide">
-            <img src="/public/images/client.png" alt="client" />
-            <p>Vel officiis dolor ea illo aut eligendi ullam non laudantium magnam et recusandae molestiae sit iure unde aut voluptate quaerat. Id sunt provident quo possimus impedit vel doloremque obcaecati qui ullam consectetur et ipsum omnis.</p>
-            <span>Christine Beckam - Designer</span>
-          </SwiperSlide>
-          <SwiperSlide className="clients--slide">
-            <img src="/public/images/client.png" alt="client" />
-            <p>Vel officiis dolor ea illo aut eligendi ullam non laudantium magnam et recusandae molestiae sit iure unde aut voluptate quaerat. Id sunt provident quo possimus impedit vel doloremque obcaecati qui ullam consectetur et ipsum omnis.</p>
-            <span>Christine Beckam - Designer</span>
-          </SwiperSlide>
-        </Swiper>
+        <div className="clients--swiper">
+          <Swiper
+            spaceBetween={30}
+            slidesPerView={1}
+            cssMode={true}
+            navigation={true}
+            className="clients--slides"
+            pagination={{ clickable: true }}
+            modules={[Navigation, Pagination]}
+          >
+            <SwiperSlide className="clients--slide">
+              <img src="/images/client.png" alt="client" />
+              <div className="clients--slide__info">
+                <p>Vel officiis dolor ea illo aut eligendi ullam non laudantium magnam et recusandae molestiae sit iure unde aut voluptate quaerat. Id sunt provident quo possimus impedit vel doloremque obcaecati qui ullam consectetur et ipsum omnis.</p>
+                <span>Christine Beckam - Designer</span>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="clients--slide">
+              <img src="/images/client.png" alt="client" />
+              <div className="clients--slide__info">
+                <p>Vel officiis dolor ea illo aut eligendi ullam non laudantium magnam et recusandae molestiae sit iure unde aut voluptate quaerat. Id sunt provident quo possimus impedit vel doloremque obcaecati qui ullam consectetur et ipsum omnis.</p>
+                <span>Christine Beckam - Designer</span>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="clients--slide">
+              <img src="/images/client.png" alt="client" />
+              <div className="clients--slide__info">
+                <p>Vel officiis dolor ea illo aut eligendi ullam non laudantium magnam et recusandae molestiae sit iure unde aut voluptate quaerat. Id sunt provident quo possimus impedit vel doloremque obcaecati qui ullam consectetur et ipsum omnis.</p>
+                <span>Christine Beckam - Designer</span>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </section>
   )
